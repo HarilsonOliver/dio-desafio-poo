c7353eb0da03d3a1f033e95882ce3df6dd111b6e
HP <harilsonpires@gmail.com>
2024-11-12 15:16:59 -0300

    first commit

---
+++ sys-smartphones/Models/Iphone.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nome}\" no iPhone");
+        }
+    }
+}
---
+++ sys-smartphones/Models/Smartphone.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; private set; }
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
+
+        protected Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nome);
+    }
+}
